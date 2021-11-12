9a93ba4c849dc2f203be29521ae47dde7bb7096a
Phillip Boswell <phillip.boswell@gmail.com>
2021-11-12 12:02:33 -0800

    Project 1 push for Instructors

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,3 +1,22 @@
 window.alert ("Hello World");
 
 document.write("hello world");
+
+var A = "<br> I'm an Adam";
+var B = "OH NOS YOUR HERE!";
+var C = "<br>I'm goint to use escape to put this quote in, \"Sea shells, sea shells, by the sea shore\" ";
+var D = "<br>Bruh " + "You coming ";
+var E = "it's this way<br>"
+var Top = "Bread", Middle =" Peanut and Jelly", Bottom=" more Bread";
+var Top = Top.fontcolor("red");
+var Middle = Middle.fontcolor("purple");
+var Bottom = Bottom.fontcolor("orange");
+var Expresion = 5+5; //this is considerd an expressoin. an expression is a combination of values that are computed by the computer
+
+
+document.write(A); //this is a statment
+document.write(C);
+window.alert(B);
+document.write(D+E);
+document.write(Top+Middle+Bottom)// this has been concatenated
+
