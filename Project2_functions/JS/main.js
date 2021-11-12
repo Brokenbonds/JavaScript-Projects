2a57f8eda225cbf58180ec23f9b6a2bb0db67c5c
Phillip Boswell <phillip.boswell@gmail.com>
2021-11-12 15:25:56 -0800

    I've added some comments

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,10 +1,12 @@
-var X = "Adam"
-var Y = "Tonya"
-var Space = " "
+var X = "Adam"; // these are un-typed string variables
+var Y = "Tonya";
+var Space = " ";
 
+
+// this here is a function for out puting text
 function text(){
     var dating= X+" and ";
-    dating += Y+Space;
+    dating += Y+Space; // this is concatenating 2 strings togther
     var Annoying= dating +"sitting in a tree! K.I.S.S.I.N.G"
     document.getElementById("annoyingKidsSong").innerHTML = Annoying;
 }
