c8b1e28bedcb109f61452465577d191cc5cb1f2b
Phillip Boswell <phillip.boswell@gmail.com>
2021-11-22 17:23:03 -0800

    More assignments

---
+++ Other Assignments/JS/DataAtributes.js
@@ -0,0 +1,8 @@
+function displayType(characters){
+    var characterType= characters.getAttribute("data-character-type")
+    alert(characterType+" is in the " +characters.innerHTML + " universe.")
+}
+
+function test(){
+    alert("test");
+}
